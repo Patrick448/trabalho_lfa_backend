36a16b75f118c60660b731fd1095abf3bfd2d8dd
Patrick448 <patrickcarvalho448@gmail.com>
2022-07-16 18:20:55 -0300

    Update SecurityConfig.java

--- src/main/java/lfa/trabalho_lfa/config/SecurityConfig.java
+++ src/main/java/lfa/trabalho_lfa/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 */
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/afd/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/afd/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/afn/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/user/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
