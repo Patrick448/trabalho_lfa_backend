da73560aef632eac06392e52489f8d272445ba1c
Vitoria Natalia Caetano <vitorianatalia@ice.ufjf.br>
2022-07-15 23:06:37 -0300

    loading effect

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -52,8 +52,6 @@ function handleSubmit() {
     }
 }
 
-
-
 function sendXML(xml, option) {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "text/plain");
@@ -71,9 +69,32 @@ function sendXML(xml, option) {
 
 async function postReq(requestOptions, option) {
     console.log(option)
+    loadingScreen(true);
     await fetch(`https://trabalho-lfa.herokuapp.com/${option}/load-${option}?uuid=${user_session}`, requestOptions)
         .then(response => response.text())
         .then(result => displayTupla(result))
         .catch(error => console.log('error', error));
+    loadingScreen(false)
 }
 
+function loadingScreen(mode) {
+    if (mode) {
+        document.querySelector('.lds-roller').style.visibility='visible';
+        document.body.style.overflow = "hidden";
+    }
+    else {
+        document.querySelector('.lds-roller').style.visibility='hidden';
+        document.body.style.overflow = "auto";
+    }
+}
+
+// document.getElementById('automata-word').addEventListener('input', handleWordTyped)
+
+// function handleWordTyped(event) {
+//     console.log(event)
+//     var userWord = event.target.value;
+//     await fetch(`https://trabalho-lfa.herokuapp.com/${option}/accepts?uuid=${user_session}&word=${userWord}`, requestOptions)
+//         .then(response => response.text())
+//         .then(result => console.log(result))
+//         .catch(error => console.log('error', error));
+// }
